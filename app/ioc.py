d95a8f23d89eef92ba96ed786ef847dd447f6904
Artur Shiriev <lesnik512@gmail.com>
2024-04-21 17:55:29 +0300

    update DI package

--- app/ioc.py
+++ app/ioc.py
@@ -1,3 +1,6 @@
+import typing
+
+from sqlalchemy.ext.asyncio import AsyncEngine
 from that_depends import BaseContainer, providers
 
 from app.db.resource import create_sa_engine, create_session
@@ -6,7 +9,7 @@ from app.repositories.decks import CardsRepository, DecksRepository
 
 class IOCContainer(BaseContainer):
     database_engine = providers.AsyncResource(create_sa_engine)
-    session = providers.AsyncContextResource(create_session, engine=database_engine)
+    session = providers.AsyncContextResource(create_session, engine=typing.cast(AsyncEngine, database_engine))
 
     decks_repo = providers.Factory(DecksRepository, session=session)
     cards_repo = providers.Factory(CardsRepository, session=session)
