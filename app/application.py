62f4c77d69cb0e1143fcae9e27ac37d3dad8f7ba
Artur Shiriev <lesnik512@gmail.com>
2024-05-17 21:21:15 +0300

    add lifespan manager

--- app/application.py
+++ app/application.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+import contextlib
+import typing
+
+import fastapi
 from that_depends.providers import DIContextMiddleware
 
 from app import exceptions, ioc
@@ -6,19 +9,31 @@ from app.api.decks import ROUTER
 from app.exceptions import DatabaseValidationError
 
 
-def get_app() -> FastAPI:
-    settings = ioc.IOCContainer.settings.sync_resolve()
-    _app = FastAPI(
-        title=settings.service_name,
-        debug=settings.debug,
-    )
-    _app.include_router(ROUTER, prefix="/api")
-    _app.add_middleware(DIContextMiddleware)
-    _app.add_exception_handler(
-        DatabaseValidationError,
-        exceptions.database_validation_exception_handler,  # type: ignore[arg-type]
-    )
-    return _app
+def include_routers(app: fastapi.FastAPI) -> None:
+    app.include_router(ROUTER, prefix="/api")
 
 
-application = get_app()
+class AppBuilder:
+    def __init__(self) -> None:
+        self.settings = ioc.IOCContainer.settings.sync_resolve()
+        self.app: fastapi.FastAPI = fastapi.FastAPI(
+            title=self.settings.service_name,
+            debug=self.settings.debug,
+            lifespan=self.lifespan_manager,
+        )
+        include_routers(self.app)
+        self.app.add_middleware(DIContextMiddleware)
+        self.app.add_exception_handler(
+            DatabaseValidationError,
+            exceptions.database_validation_exception_handler,  # type: ignore[arg-type]
+        )
+
+    @contextlib.asynccontextmanager
+    async def lifespan_manager(self, _: fastapi.FastAPI) -> typing.AsyncIterator[dict[str, typing.Any]]:
+        try:
+            yield {}
+        finally:
+            await ioc.IOCContainer.tear_down()
+
+
+application = AppBuilder().app
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,13 @@
 import runpy
 from unittest import mock
 
+import fastapi
 import pytest
+from that_depends.providers import container_context
 
 from app import __main__ as api_main
 from app import ioc
+from app.application import AppBuilder
 
 
 async def test_init_resources() -> None:
@@ -18,3 +21,9 @@ async def test_init_resources() -> None:
 def test_main(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr("granian.Granian", mock.Mock())
     runpy.run_module(api_main.__name__, run_name="__main__")
+
+
+async def test_app_lifespan() -> None:
+    ioc.IOCContainer.reset_override()
+    async with AppBuilder().lifespan_manager(fastapi.FastAPI()), container_context():
+        await ioc.IOCContainer.session()
