73b917b122f9fe947ffa6de436ee4e6c84e3246c
Artur Shiriev <lesnik512@gmail.com>
2024-05-19 23:06:15 +0300

    update that-depends

--- app/ioc.py
+++ app/ioc.py
@@ -1,6 +1,3 @@
-import typing
-
-from sqlalchemy.ext.asyncio import AsyncEngine
 from that_depends import BaseContainer, providers
 
 from app.db.resource import create_sa_engine, create_session
@@ -11,8 +8,8 @@ from app.settings import Settings
 class IOCContainer(BaseContainer):
     settings = providers.Singleton(Settings)
 
-    database_engine = providers.AsyncResource(create_sa_engine, settings=typing.cast(Settings, settings))
-    session = providers.AsyncContextResource(create_session, engine=typing.cast(AsyncEngine, database_engine))
+    database_engine = providers.AsyncResource(create_sa_engine, settings=settings.cast)
+    session = providers.AsyncContextResource(create_session, engine=database_engine.cast)
 
     decks_repo = providers.Factory(DecksRepository, session=session)
     cards_repo = providers.Factory(CardsRepository, session=session)
