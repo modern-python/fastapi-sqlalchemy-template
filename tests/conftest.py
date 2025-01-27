cf11493e96c34f65b4ada28d3c531fbfef43967f
Artur Shiriev <lesnik512@gmail.com>
2025-01-27 19:00:04 +0300

    add di_container ficture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import typing
 
+import modern_di
 import modern_di_fastapi
 import pytest
 from httpx import ASGITransport, AsyncClient
@@ -18,19 +19,25 @@ async def client() -> typing.AsyncIterator[AsyncClient]:
         yield client
 
 
-@pytest.fixture(autouse=True)
-async def db_session() -> typing.AsyncIterator[AsyncSession]:
-    async with modern_di_fastapi.fetch_di_container(application) as di_container:
-        engine = await ioc.Dependencies.database_engine.async_resolve(di_container)
-        connection = await engine.connect()
-        transaction = await connection.begin()
-        await connection.begin_nested()
-        ioc.Dependencies.database_engine.override(connection, di_container)
+@pytest.fixture
+async def di_container() -> typing.AsyncIterator[modern_di.Container]:
+    di_container_: typing.Final = modern_di_fastapi.fetch_di_container(application)
+    async with di_container_:
+        yield di_container_
 
-        try:
-            yield AsyncSession(connection, expire_on_commit=False, autoflush=False)
-        finally:
-            if connection.in_transaction():
-                await transaction.rollback()
-            await connection.close()
-            await engine.dispose()
+
+@pytest.fixture(autouse=True)
+async def db_session(di_container: modern_di.Container) -> typing.AsyncIterator[AsyncSession]:
+    engine = await ioc.Dependencies.database_engine.async_resolve(di_container)
+    connection = await engine.connect()
+    transaction = await connection.begin()
+    await connection.begin_nested()
+    ioc.Dependencies.database_engine.override(connection, di_container)
+
+    try:
+        yield AsyncSession(connection, expire_on_commit=False, autoflush=False)
+    finally:
+        if connection.in_transaction():
+            await transaction.rollback()
+        await connection.close()
+        await engine.dispose()
