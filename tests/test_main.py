40f54dbcaec3e1efa644d253730b9b2754b25a59
Artur Shiriev <lesnik512@gmail.com>
2025-04-19 13:58:44 +0300

    update modern-di

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,22 +1,15 @@
 import runpy
 from unittest import mock
 
-import fastapi
 import modern_di
 import pytest
 
 from app import ioc
-from app.application import AppBuilder
 
 
 def test_main(monkeypatch: pytest.MonkeyPatch) -> None:
     monkeypatch.setattr("granian.Granian", mock.Mock())
     runpy.run_module("app.__main__", run_name="__main__")
-
-
-async def test_app_lifespan() -> None:
-    async with AppBuilder().lifespan_manager(fastapi.FastAPI()):
-        pass
 
 
 async def test_session() -> None:
